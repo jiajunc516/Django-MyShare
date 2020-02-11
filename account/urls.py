1ca07f31531d5edbd28ba5d8b05acb05f383784a
jiajunchang <jiajunc@uci.edu>
2020-02-11 05:19:30 -0800

    Create Logout

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,13 @@ from . import views
 urlpatterns = [
     # post views
     # path("login/", views.user_login, name="login"),
-    path("login/", auth_views.LoginView.as_view(template_name="account/login.html"), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="account/login.html"),
+        name="login"),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="account/logout.html"),
+        name="logout"),
     path("", views.dashboard, name="dashboard"),
 ]
