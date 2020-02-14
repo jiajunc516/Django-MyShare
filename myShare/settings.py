a1a403a92b27c34336fdb9bfb8204f13f0e67aea
jiajunchang <jiajunc@uci.edu>
2020-02-14 05:20:50 -0800

    Add image database

--- myShare/settings.py
+++ myShare/settings.py
@@ -59,7 +59,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account.apps.AccountConfig',
-    'social_django'
+    'social_django',
+    'image.apps.ImageConfig',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
